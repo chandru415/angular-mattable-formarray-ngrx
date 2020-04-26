92e60b24787dca09d71f8d379617d9debb1477e1
chandru415 <chandru9052@gmail.com>
2020-04-26 17:59:53 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('src/app/grid-list/grid-list.module').then(m => m.GridListModule) }
+  // { path: '', loadChildren: () => import('src/app/grid-list/grid-list.module').then(m => m.GridListModule) }
 ];
 
 @NgModule({
